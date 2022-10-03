e2ee8512943b6b1ee17aa817dd2c75e46a37f395
plate <plate@noreply.localhost>
2022-10-04 00:59:28 +0100

    switch to runit services I switched to runit so if i ever share the playbook, it's easier to get setup

---
+++ dendrite/scripts/service_dirs.sh
@@ -0,0 +1,4 @@
+#!/data/data/com.termux/files/usr/bin/sh
+mkdir -p $PREFIX/var/service/dendrite/log/
+ln -sf $PREFIX/share/termux-services/svlogger $PREFIX/var/service/dendrite/log/run
+
--- dendrite/templates/monolith.sh
+++ dendrite/templates/monolith.sh
@@ -1,3 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/sh
 cd {{ install_dir }}
-exec ./bin/dendrite-monolith-server --config ./dendrite.yaml -really-enable-open-registration
+exec ./bin/dendrite-monolith-server --config ./dendrite.yaml -really-enable-open-registration 2>&1
